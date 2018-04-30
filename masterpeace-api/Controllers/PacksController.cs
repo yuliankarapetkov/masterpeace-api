221ae7d9be2d8ca31b6e415e0178265f5fef1b3c
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-04-30 16:37:49 +0100

    Add project files.

---
+++ masterpeace-api/Controllers/PacksController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace masterpeace_api.Controllers
+{
+    public class Pack
+    {
+        public string Name { get; set; }
+        public int SessionsCount { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    [Produces("application/json")]
+    [Route("api/packs")]
+    public class PacksController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Pack> Get()
+        {
+            return new Pack[] {
+                new Pack()
+                {
+                    Name = "Тревожност",
+                    SessionsCount = 30,
+                    IsCompleted = false
+                },
+                new Pack()
+                {
+                    Name = "Щастие",
+                    SessionsCount = 10,
+                    IsCompleted = false
+                },
+                new Pack()
+                {
+                    Name = "Стрес",
+                    SessionsCount = 30,
+                    IsCompleted = true
+                }
+            };
+        }
+    }
+}
